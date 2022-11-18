07440880606e341c210315e11f72f4d1bd997a05
nbayashi <naoya_nstyle@hotmail.co.jp>
2022-11-18 21:45:26 +0900

    update form size

--- web/components/melocules/PanelTwo/index.tsx
+++ web/components/melocules/PanelTwo/index.tsx
@@ -6,7 +6,7 @@ const PanelTwo: React.FC = () => {
       <iframe
         src="https://8336c20d.form.kintoneapp.com/public/000870d4bdd6bda9886dedc59e1f12e66cf2bc66d46309766eb6f97dfd211dd3?iframe=true"
         frameBorder="0"
-        style={{ position: "absolute", height: "100%", width: "100%" }}
+        style={{ position: "absolute", height: "calc(100%/0.8)", width: "calc(100%/0.8)", transform: "scale(0.8)", transformOrigin: "0 0" }}
       ></iframe>
     </Card>
   );
